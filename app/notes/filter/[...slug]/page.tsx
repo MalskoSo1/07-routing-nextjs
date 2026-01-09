187a49db33a69397a7e7f1232bb0651b5b8709d7
Ліза Гоцко <filani9818@gmail.com>
2026-01-09 23:12:03 +0200

    3

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,4 +1,4 @@
-import NotesClient from "../../Notes.client";
+import NotesClient from "./Notes.client";
 
 interface NotesFilterProps {
   params: Promise<{ slug?: string[] }>;
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NotesClient from "./Notes.client";
+import NotesClient from "./filter/[...slug]/Notes.client";
 
 const NotesPage = async () => {
   const queryClient = new QueryClient();
